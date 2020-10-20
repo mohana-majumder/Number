fc456691ea32ee6faa70da2fa554b1198c79a2b8
mohanamajumder <mohana.majumder@cyber-swift.com>
2020-10-21 01:08:03 +0530

    initial commit

---
+++ Number/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Enter a number : ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Program p = new Program();
+                string str = p.DivNum(num);
+                Console.WriteLine(str);
+                Console.WriteLine("Do you want to continue (Y/N)? ");
+            } while (Console.ReadLine().ToUpper() == "Y");
+        }
+        public string DivNum(int num)
+        {
+            string str = "";
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                str = "C";
+            }
+            else if (num % 3 == 0)
+            {
+                str = "A";
+            }
+            else if (num % 5 == 0)
+            {
+                str = "B";
+            }
+            else
+            {
+                str = "Not divisible";
+            }
+            return str;
+        }
+    }
+}
---
+++ Number/Test.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Number
+{
+    [TestFixture]
+    class Test
+    {
+        [TestCase]
+        public void DivByThree()
+        {
+            int num = 3;
+            Program p = new Program();
+            string str = p.DivNum(num);
+            Assert.AreEqual("A", str);
+        }
+        [TestCase]
+        public void DivByFive()
+        {
+            int num = 5;
+            Program p = new Program();
+            string str = p.DivNum(num);
+            Assert.AreEqual("B", str);
+        }
+        [TestCase]
+        public void DivByThreeAndFive()
+        {
+            int num = 15;
+            Program p = new Program();
+            string str = p.DivNum(num);
+            Assert.AreEqual("C", str); 
+        }
+        [TestCase]
+        public void NotDiv()
+        {
+            int num = 7;
+            Program p = new Program();
+            string str = p.DivNum(num);
+            Assert.AreEqual("Not divisible", str);
+        }
+    }
+}
